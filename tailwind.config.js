72eb29e78931abc9ccb1220f23e13ea3f81760ab
Bárbara <benboxuwu@gmail.com>
2026-01-30 00:52:51 -0600

    Primer commit: sitio web SUMYSE completo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'sumyse-blue-dark': '#1F2A43',
+        'sumyse-blue-light': '#334155',
+        'sumyse-orange': '#F97316',
+        'sumyse-bg': '#E5E7EB',
+      },
+    },
+  },
+  plugins: [],
+}
